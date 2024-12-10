fb4f254727f39bbf3b096af2e82dff138d4a96b1
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2024-12-10 23:16:49 +0100

    Fix main.

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,20 @@ use tasks::Task;
 
 pub fn run_tasks(mut from: Task, to: Task) {
     let files = files::Files::from_env();
-    while from != to {
+    loop  {
         println!(
-            "{from}: {}",
+            "{from} result: {}",
             from.run(&files.get_input(files::FilesType::Task, &from))
         );
+        if from == to {
+            break
+        }
         from = from.next()
     }
 }
 
 pub fn mesure_tasks(mut from: Task, to: Task) {
+    println!("Run tasks from: {}, to: {}", from, to);
     let files = files::Files::new(PathBuf::from(env::var("AOC_DATA").unwrap()));
     let mut threads: Vec<JoinHandle<()>> = Vec::new();
     let now = Instant::now();
@@ -38,6 +42,5 @@ pub fn mesure_tasks(mut from: Task, to: Task) {
         thread.join().unwrap()
     }
     let elapsed = now.elapsed();
-    println!("Run tasks from: {}, to: {}", from, to);
     println!("Elapsed time: {:?}", elapsed);
 }
